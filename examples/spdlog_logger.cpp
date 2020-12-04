d398ab7ff4676d67a3c93fa027c3124f930ae8f9
ngrodzitski <utromvecherom@gmail.com>
2020-12-04 22:32:32 +0300

    Add custom pattern for spdlog example.

--- examples/spdlog_logger.cpp
+++ examples/spdlog_logger.cpp
@@ -20,9 +20,12 @@
 
 auto make_logger()
 {
+    auto sink = std::make_shared< spdlog::sinks::stdout_sink_st >();
+    sink->set_pattern("[%Y-%m-%d %T][%n][%l] %v [%g]");
+
     return logr::spdlog_logger_t<>{
         "console",
-        std::make_shared< spdlog::sinks::stdout_sink_st >(),
+        std::move( sink ),
         logr::log_message_level::trace
     };
 }
