1df2b74c7521597e97d5356417dae11c68c313ec
ngrodzitski <utromvecherom@gmail.com>
2020-12-11 20:17:07 +0300

    Minor changes for packaging.

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+
 #include <logr/ostream_backend.hpp>
+#include <logr/config.hpp>
 
 int main()
 {
     logr::ostream_logger_t<> logger{ std::cout,
                                      logr::log_message_level::trace };
 
-    logger.info( "Welcome logr provided with conan!" );
+    logger.info( []( auto & out ){
+        fmt::format_to( out,
+                        "Welcome to logr (v{}.{}.{}) provided with conan!",
+                        LOGR_VERSION_MAJOR,
+                        LOGR_VERSION_MINOR,
+                        LOGR_VERSION_PATCH );
+    } );
 }
